c67308b34a2b8d3523ce6ede326815e73b628535
Derukoo <derkkouhai@gmail.com>
2024-12-27 12:58:21 +0100

    More media queries.

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener("scroll", handleScroll);
   handleScroll(); // Run once on page load to catch visible sections
 });
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+  const hnav = document.getElementById('hamburger-nav');
+  const dnav = document.getElementById('desktop-nav');
+  const bnav = document.getElementById('bottom-nav');
+	if (scroll_position > 200) {
+		hnav.style.backgroundColor = '#33485b';
+		dnav.style.backgroundColor = '#33485b';
+
+  } else {
+    hnav.style.backgroundColor = '#192530';
+    dnav.style.backgroundColor = '#192530';
+
+  }
+});
